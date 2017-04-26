854430689a18b38e04a924d630b0d8a42c41444a
Lukas Reschke <lukas@statuscode.ch>
2017-04-26 14:49:11 +0200

    To tabs
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- lib/Controller/LogoutController.php
+++ lib/Controller/LogoutController.php
@@ -12,14 +12,14 @@ use OCP\IUserManager;
 use OCP\IUserSession;
 
 class LogoutController extends Controller {
-    /** @var IUserManager */
-    private $userManager;
-    /** @var IUserSession */
-    private $userSession;
-    /** @var ILogger */
-    private $logger;
-    /** @var ISession */
-    private $session;
+	/** @var IUserManager */
+	private $userManager;
+	/** @var IUserSession */
+	private $userSession;
+	/** @var ILogger */
+	private $logger;
+	/** @var ISession */
+	private $session;
 
 	/**
 	 * @param string $appName
@@ -29,39 +29,39 @@ class LogoutController extends Controller {
 	 * @param ISession $session
 	 * @param ILogger $logger
 	 */
-    public function __construct($appName,
+	public function __construct($appName,
 								IRequest $request,
 								IUserManager $userManager,
 								IUserSession $userSession,
 								ISession $session,
 								ILogger $logger) {
-        parent::__construct($appName, $request);
-        $this->userManager = $userManager;
-        $this->userSession = $userSession;
-        $this->session = $session;
-        $this->logger = $logger;
-    }
+		parent::__construct($appName, $request);
+		$this->userManager = $userManager;
+		$this->userSession = $userSession;
+		$this->session = $session;
+		$this->logger = $logger;
+	}
 
-    /**
-     * @UseSession
+	/**
+	 * @UseSession
 	 * @NoAdminRequired
-     *
-     * @param string $userId
-     * @return JSONResponse
-     */
-    public function logout($userId) {
+	 *
+	 * @param string $userId
+	 * @return JSONResponse
+	 */
+	public function logout($userId) {
 		$user = $this->session->get('oldUserId');
 		$user = $this->userManager->get($user);
 
-        if($user === null) {
-        	return new JSONResponse(
-        		sprintf(
-        			'No user found for %s',
+		if($user === null) {
+			return new JSONResponse(
+				sprintf(
+					'No user found for %s',
 					$userId
 				),
 				Http::STATUS_NOT_FOUND
 			);
-        }
+		}
 
 		$this->userSession->setUser($user);
 
