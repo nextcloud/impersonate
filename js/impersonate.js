be370d447b655239be3f37002b518f9bd5fd736b
Dominik Prodinger <prodingerd@users.noreply.github.com>
2017-10-16 21:18:28 +0200

    Move Impersonate-Button Into Dropdown
    This commit addresses two issues:
    
    1. Impersonate-button should be moved to the dropdown menu (where disable, delete options are located)
    2. Impersonation of yourself makes no sense and since the dropdown is only shown for users other than yourself, impersonation of yourself is no longer possible.

--- js/impersonate.js
+++ js/impersonate.js
@@ -1,42 +1,35 @@
 (function(){
+    $(document).ready(function() {
+        if(!OC.isUserAdmin()) {
+            return;
+        }
 
-	$(document).ready(function() {
-		if(!OC.isUserAdmin()) {
-			return;
-		}
+        function impersonate(userId) {
+            $.post(
+                OC.generateUrl('apps/impersonate/user'), 
+                { userId: userId }
+            ).done(function() {
+                window.location = OC.generateUrl('apps/files');
+            }).fail(function( result ) {
+                OC.dialogs.alert(result.responseJSON.message, t('impersonate', 'Could not impersonate user'));
+            });
+        }
 
-		function impersonate(userId) {
-			$.post(
-				OC.generateUrl('apps/impersonate/user'),
-				{ userId: userId }
-			).done(function() {
-				window.location = OC.generateUrl('apps/files');
-			}).fail(function( result ) {
-				OC.dialogs.alert(result.responseJSON.message, t('impersonate', 'Could not impersonate user'));
-			});
-		}
-		var $newColumn = $("#userlist").find("tr:first-child");
-		$('<th id="impersonateId" scope="col">'+t('impersonate', 'Impersonate') +'</th>').insertAfter($newColumn.find("#headerName"));
-		$('<td><a class="action permanent impersonate" href="#" title="' +
-			t('impersonate', 'Impersonate') + '">' +
-			'<img class="svg permanent action" src="' + OC.imagePath('core','actions/user.svg') + '" />' +
-			'</a></td>')
-			.insertAfter('#userlist .name');
+        $('<li><a href="#" class="menuitem permanent impersonate">' +
+            '<span class="icon icon-user"></span><span>Impersonate</span></a></li>').insertAfter(
+                $(".userActionsMenu").find("li:last-child"));
 
-		$('#userlist').on('click', '.impersonate', function() {
-			var userId = $(this).parents('tr').find('.name').text();
-			OCdialogs.confirm(
-				t('impersonate', 'Are you sure you want to impersonate "{userId}"?', {userId: userId}),
-				t('impersonate', 'Impersonate user' ),
-				function(result) {
-					if (result) {
-						impersonate(userId);
-					}
-				},
-				true
-			);
-		});
-
-	});
-
+        $('body').on('click', '.impersonate', function() {
+            var userId = $(this).parents('tr').find('.name').text();
+            OCdialogs.confirm(
+                t('impersonate', 'Are you sure you want to impersonate "{userId}"?', {userId: userId}),
+                t('impersonate', 'Impersonate user'),
+                function(result) {
+                    if (result) {
+                        impersonate(userId);
+                    }
+                }, true
+            );
+        });
+    });
 })();
